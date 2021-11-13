f96d85c020377f1d8e47f31af6507916aba3be4e
TatyanaLikar <85054268+TanyanaLikar@users.noreply.github.com>
2021-11-13 09:38:43 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+const bodyEl = document.querySelector('body')
+const startBtn= document.querySelector('button[data-start]');
+const stopBtn= document.querySelector('button[data-stop]');
+// console.log(bodyEl)
+// console.log(btnStart)
+// console.log(btnStop)
+stopBtn.disabled = true;
+startBtn.addEventListener("click", startClick);
+stopBtn.addEventListener('click', stopClik)
+ function startClick(){
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    timerId = setInterval(() => {
+        const color = getRandomHexColor()
+        bodyEl.style.backgroundColor = color;
+      }, 1000);
+    };
+
+    function stopClik(){
+        startBtn.disabled = false;
+        stopBtn.disabled = true;
+        clearTimeout(timerId);
+    }
+  
 
